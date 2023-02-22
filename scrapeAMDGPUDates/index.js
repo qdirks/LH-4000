8478867b3aabcc9ba41b80c6969235ba1d67c3ec
Quinn Dirks <qdirks@gmail.com>
2023-02-22 01:17:57 -0600

    save

--- scrapeAMDGPUDates/index.js
+++ scrapeAMDGPUDates/index.js
@@ -1,4 +1,5 @@
 (function() {
+    console.clear();
     const mw_parser_output = document.querySelector("#mw-content-text .mw-parser-output");
 
     // split the children into sections
@@ -9,40 +10,29 @@
     }, [[]]);
 
     /** @type {HTMLTableElement[]} */
-    const tables = sections.find(section=>section[0].textContent.startsWith("Desktop GPUs"))
-    .filter(el=>el.nodeName === 'TABLE');
-
-    // let's try going through the tables backwards so that we start with the latest cards
-
-    // last 10 for nvidia
-    // last 10 for amd
+    const tables = sections
+    .reduce((pv, cv)=>pv.concat(cv), [])
+    .filter(el=>el.nodeName === 'TABLE')
+    .slice(2); // skip the first two tables, which are not graphics cards tables
 
     console.log(tables.length);
 
-    const gpuList = tables.reverse().slice(0, 26).reverse().reduce((pv, table)=>{
-
+    const gpuList = tables
+    // .reverse()
+    .slice(5, 6)
+    // .reverse()
+    .reduce((pv, table, ix)=>{
+        console.log("table:", ix);
         table = normalizeTable(table);
 
         // right now, I think I can check if a row contains data by checking for the presence of a td element in the last child position.
-        function walkTree(node) {
-            let acc = '';
-            if (node.nodeName === 'BR') return ' ';
-            else if (node.nodeType === Node.TEXT_NODE) acc = node.textContent;
-            else for (var ix = 0; ix < node.childNodes.length; ix++) {
-                let node_ = node.childNodes[ix];
-                if (node_.nodeName === 'SUP') continue;
-                acc += walkTree(node_);
-            }
-            return acc.trim();
-        }
-        [].slice.call(table.querySelectorAll("tbody tr")).forEach(row=>{
-            if (row.children[row.children.length - 1].nodeName !== 'TD') return; // skip non data row
-            const gpu = {
+        [].slice.call(table.querySelectorAll("tbody .table-rh")).map(rh=>rh.parentElement)
+        .forEach(row=>row.children[row.children.length - 1].nodeName === 'TD'
+            && pv.push({
                 name: walkTree(row.children[0]),
                 date: walkTree(row.children[1])
-            };
-            pv.push(gpu);
-        });
+            })
+        );
         return pv;
     }, []);
 
@@ -50,27 +40,32 @@
 
     /** @param {HTMLTableElement} table */
     function normalizeTable(table) {
-        // make a copy
-        // table = table.cloneNode(true);
-
-        // take out the row span crap
-        /** @type {HTMLTableRowElement[]} */
-        let rows = [].slice.call(table.querySelectorAll("tbody tr"));
 
         // the first row sets the number of cells
-        const width = [].slice.call(rows[0].children).reduce((pv, th)=>{
+        const width = [].slice.call(table.querySelector("tr").children).reduce((pv, th)=>{
             if (th.colSpan) return pv += th.colSpan;
             else return pv += 1;
         }, 0);
 
-        rows = rows.filter(row=>row.children[row.children.length - 1].nodeName === 'TD');
+
+        // all rows where there is a th at the beginning with class of .table-rh
+        // or there is a td at the beginning
+        /** @type {HTMLTableRowElement[]} */
+        const rows = [].slice.call(table.querySelectorAll('tr'))
+        .filter(tr=>{
+            /** @type {HTMLTableRowElement} */
+            const row = tr;
+            if (row.children[0].classList.contains("table-rh")) return true;
+            else if (row.children[0].nodeName === 'TD') return true;
+            else return false;
+        });
 
         for (let tdIndex = 0; tdIndex < width; tdIndex++) {
             for (let rowIndex = 0; rowIndex < rows.length; rowIndex++) {
                 let rowOuter = rows[rowIndex];
                 let td = rowOuter.children[tdIndex];
                 if (!td) continue;
-                if (!td.rowSpan) continue;
+                if (td.rowSpan === 1) continue;
                 let rowSpan = td.rowSpan;
                 td.removeAttribute('rowSpan');
                 for (let ix = 1; ix < rowSpan; ix++) {
@@ -82,7 +77,18 @@
             }
         }
 
-
         return table;
+    }
+    /** @param {HTMLElement} node */
+    function walkTree(node) {
+        let acc = '';
+        if (node.nodeName === 'BR') return ' ';
+        else if (node.nodeType === Node.TEXT_NODE) acc = node.textContent;
+        else for (var ix = 0; ix < node.childNodes.length; ix++) {
+            let node_ = node.childNodes[ix];
+            if (node_.nodeName === 'SUP') continue;
+            acc += walkTree(node_);
+        }
+        return acc.trim();
     }
 })();
